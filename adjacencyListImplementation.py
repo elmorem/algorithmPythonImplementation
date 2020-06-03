bc93a46661dd7c0d91d3687405e3840ec7d800b2
elmorem <elmorem@gmail.com>
2020-06-03 16:40:50 -0700

    finished

--- adjacencyListImplementation.py
+++ adjacencyListImplementation.py
@@ -36,4 +36,41 @@ class Graph:
         self.vertList = {}
         self.numVerticies = 0
 
-    def addVertex 
+    def addVertex (self, key):
+        self.numVerticies = self.numVerticies +1
+        newVertex=Vertex(key)
+        self.vertList[key]= newVertex
+        return newVertex
+
+    def getVertex (self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+    
+    def __contains__(self,n):
+        return n in self.vertList
+        
+    def addEdge(self,f,t,cost=0):
+        if f not in self.vertList:
+            nv=self.addVertex(f)
+        if t not in self.vertList:
+            nv=self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def __iter__(self):
+        return iter(self.vertList.values())
+
+
+""" g=Graph()
+for i in range(6):
+    g.addVertex(i)
+
+for vertex in g:
+    print (vertex)
+    print (vertex.getConnections())
+    print ('\n')
+ """
