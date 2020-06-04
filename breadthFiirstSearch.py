9a07d805c57e17d995cbf9e187889f099eab9b18
elmorem <elmorem@gmail.com>
2020-06-04 12:52:36 -0700

    cbvncvb

--- breadthFiirstSearch.py
+++ breadthFiirstSearch.py
@@ -1,3 +1,39 @@
-"""
-starts at a vertex S color the start variable grey to show that it is being explored.  all vertecies initialized white.  when a vertex has been completely explored it goes black.
-"""
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+bfs(graph, 'A')
+
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+list(bfs_paths(graph, 'A', 'F'))
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+shortest_path(graph, 'A', 'F')
