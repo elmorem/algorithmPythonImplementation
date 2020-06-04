85e031e1d42d165a2655e905f723ec1b80d8eba1
elmorem <elmorem@gmail.com>
2020-06-04 09:38:49 -0700

    DFS

---
+++ breadthFiirstSearch.py
@@ -0,0 +1,3 @@
+"""
+starts at a vertex S color the start variable grey to show that it is being explored.  all vertecies initialized white.  when a vertex has been completely explored it goes black.
+"""
