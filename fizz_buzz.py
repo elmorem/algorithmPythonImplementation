60632f268b046d488f42d7ad61209e8fc81a1e09
elmorem <elmorem@gmail.com>
2020-06-05 08:31:24 -0700

    add dict

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -2,7 +2,6 @@
 def fizz_buzz(numbers):
     # I think that you need to use range and len here so that when it iterates through it keeps the list. if you just use for i in numbers, it will return the values as individuals
     for i in range(len(numbers)):
-        breakpoint()
         num=numbers[i]
         if num %3 ==0:
             numbers[i]="fizz"
@@ -17,6 +16,7 @@ numbers
 #USE ENUMERATE enumerate allows you to iterate through a list AND its indicies returning a tuple
 def fizz_buzz(numbers):
     # I think that you need to use range and len here so that when it iterates through it keeps the list. if you just use for i in numbers, it will return the values as individuals
+    breakpoint()
     for i, num in enumerate(numbers):
         num=numbers[i]
         if num %3 ==0:
