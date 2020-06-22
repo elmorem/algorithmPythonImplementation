91cb5e3b9ff2b3805d1662244d69e094e73f0f92
elmorem <elmorem@gmail.com>
2020-06-22 16:24:23 -0700

    assorted help

---
+++ __assortedHelpful.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+from collections import Counter
+from collections import namedtuple
+#namedtuples are useful for when you want to pass in an immuitable object
+#basically, what i'm is useful here is that you can use a tuple that is 
+# imutalbe any place that you were previouisly going to pass in a tuple 
+# but you don't want them to be able to alter the values.  A good use case 
+# for this is when you create a class and it has some parameters that you don't want changed.
--- _dictionaries.py
+++ _dictionaries.py
@@ -1,4 +1,17 @@
 # Map¶
+# The idea of a dictionary used as a hash table to get and retrieve items using keys is often referred to as a mapping. 
+# In our implementation we will have the following methods:
+# HashTable() Create a new, empty map. It returns an empty map collection.
+# put(key,val) Add a new key-value pair to the map. If the key is already in the map then replace the old value with the new value.
+# get(key) Given a key, return the value stored in the map or None otherwise.
+# del Delete the key-value pair from the map using a statement of the form del map[key].
+# len() Return the number of key-value pairs stored
+# in the map in Return True for a statement of the form key in map, if the given key is in the map, False otherwise.
+# Implementation of a Hash Table
+# In this lecture we will be implementing our own Hash Table to complete our understanding of Hash Tables and Hash Functions! Make sure to review the video lecture before this to fully understand this implementation!
+# Keep in mind that Python already has a built-in dictionary object that serves as a Hash Table, you would never actually need to implement your own hash table in Python.
+
+# Map
 # The idea of a dictionary used as a hash table to get and retrieve items using keys is often referred to as a mapping. In our implementation we will have the following methods:
 
 # HashTable() Create a new, empty map. It returns an empty map collection.
@@ -7,23 +20,7 @@
 # del Delete the key-value pair from the map using a statement of the form del map[key].
 # len() Return the number of key-value pairs stored
 # in the map in Return True for a statement of the form key in map, if the given key is in the map, False otherwise.
-
-
-Implementation of a Hash Table
-In this lecture we will be implementing our own Hash Table to complete our understanding of Hash Tables and Hash Functions! Make sure to review the video lecture before this to fully understand this implementation!
-
-Keep in mind that Python already has a built-in dictionary object that serves as a Hash Table, you would never actually need to implement your own hash table in Python.
-
-Map
-The idea of a dictionary used as a hash table to get and retrieve items using keys is often referred to as a mapping. In our implementation we will have the following methods:
-
-HashTable() Create a new, empty map. It returns an empty map collection.
-put(key,val) Add a new key-value pair to the map. If the key is already in the map then replace the old value with the new value.
-get(key) Given a key, return the value stored in the map or None otherwise.
-del Delete the key-value pair from the map using a statement of the form del map[key].
-len() Return the number of key-value pairs stored
-in the map in Return True for a statement of the form key in map, if the given key is in the map, False otherwise.
-In [1]:
+# In [1]:
 class HashTable(object):
     
     def __init__(self,size):
